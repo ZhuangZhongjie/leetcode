e4c19b0ec8a5466a599a3e2ceb8c369c5f7efae2
ZhuangZhongjie <zhuang_zhongjie@163.com>
2017-12-11 22:51:02 +0800

    Mod: adding process.

--- addtwonumbers.cpp
+++ addtwonumbers.cpp
@@ -2,6 +2,11 @@
 
 using namespace std;
 
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
 
 
 /**
@@ -14,9 +19,35 @@ using namespace std;
  */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
-    }
+	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+		ListNode * result = new ListNode(0);
+		ListNode * last = result;
+
+		while (l1 || l2) {
+			int s = last->val;
+			if (l1) {
+				s += l1->val;
+				l1 = l1->next;
+			}
+			if (l2) {
+				s += l2->val;
+				l2 = l2->next;
+			}
+
+			if (s > 10) {
+				last->val = s - 10;
+				last->next = new ListNode(1);
+			} else {
+				last->val = s;
+				last->next = new ListNode(0);
+			}
+
+			last = last->next;
+
+		}
+
+		return result;
+	}
 };
 
 
